b37a4d993a0d8cc53291fecabf84bf8c9becd755
Jessica Bohn <jbohn.developer@gmail.com>
2019-10-29 16:31:49 -0500

    Converts mult lineitems to one with corresponding quantity value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191029204633) do
+ActiveRecord::Schema.define(version: 20191029210150) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -24,7 +24,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_select 'h2','Your Pragmatic Cart'
     assert_select 'main' do
-      assert_select 'li','Programming Ruby 1.9'
+      assert_select 'li',"1 \u00D7 Programming Ruby 1.9"
     end
   end
 
